16bfa6f781376f98b1a6bfbb4a45f6f9966bbf26
NohHaklim <62823216+NohHaklim@users.noreply.github.com>
2022-09-25 18:21:47 +0900

    stream 사용법 찾아보기

---
+++ Programmers/Level_2/PRG42578.java
@@ -0,0 +1,30 @@
+package Level_2;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class PRG42578 {
+	public int solution(String[][] clothes) {
+        int answer = 1;
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String[] cloth : clothes) {
+        	String type = cloth[1];
+        	map.put(type, map.getOrDefault(type, 0) + 1);
+        }
+        
+        Iterator<Integer> it = map.values().iterator();
+        while (it.hasNext()) {
+        	answer *= it.next().intValue() + 1;
+        }
+        return answer - 1;
+    }
+}
+
+//
+//		return Arrays.stream(clothes)
+//					.collect(groupingBy(p -> p[1], mapping(p -> p[0], counting())))
+//					.values()
+//					.stream()
+//					.collect(reducing(1L, (x, y) -> x * (y + 1))).intValue() - 1;
+//
