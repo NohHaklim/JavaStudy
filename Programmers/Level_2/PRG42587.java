5875b3232f8d0d0dd3eb3f49ba2566377d918c1c
NohHaklim <62823216+NohHaklim@users.noreply.github.com>
2022-10-04 18:34:54 +0900

    Queue와 Priority Queue 연습

--- Programmers/Level_2/PRG42587.java
+++ Programmers/Level_2/PRG42587.java
@@ -5,6 +5,11 @@ import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
+/*
+ * 		@ 프린터
+ *		@ https://school.programmers.co.kr/learn/courses/30/lessons/42587
+ */
+
 class Data {
 	public int prior;
 	public boolean target = false;
