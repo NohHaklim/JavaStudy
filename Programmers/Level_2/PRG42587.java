1752a57a5c0195b8a50fcbdc0b9305d189e7fde1
NohHaklim <62823216+NohHaklim@users.noreply.github.com>
2022-10-04 18:34:21 +0900

    Queue와 Priority Queue 활용

---
+++ Programmers/Level_2/PRG42587.java
@@ -0,0 +1,48 @@
+package Level_2;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class Data {
+	public int prior;
+	public boolean target = false;
+	
+	public Data(int prior) {
+		this.prior = prior;
+	}
+}
+
+public class PRG42587 {
+	public int solution(int[] priorities, int location) {
+       
+        Queue<Data> q = new LinkedList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int i = 0; i < priorities.length; i++) {
+        	Data data = new Data(priorities[i]);
+        	if (i == location) {
+        		data.target = true;
+        	}
+        	q.offer(data);
+        	pq.add(priorities[i]);
+        }
+        
+        int cnt = 0;
+        while (!q.isEmpty()) {
+        	Data data = q.poll();
+        	if (data.prior == pq.peek()) {
+        		pq.poll();
+        		cnt++;
+        		if (data.target) {
+        			break;
+        		}
+        	} else {
+        		q.offer(data);
+        	}
+        }
+        
+        return cnt;
+    }
+}
