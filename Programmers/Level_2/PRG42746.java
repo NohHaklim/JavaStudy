18e3ff57a91d4d00c839e31d60057d708db0a315
NohHaklim <62823216+NohHaklim@users.noreply.github.com>
2022-10-05 20:39:41 +0900

    Comparator 연습 (★)

---
+++ Programmers/Level_2/PRG42746.java
@@ -0,0 +1,41 @@
+package Level_2;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/*
+ * 		@ 가장 큰 수
+ *		@ https://school.programmers.co.kr/learn/courses/30/lessons/42746
+ *		@ 1의 자리 숫자가 가장 큰 순서로 정렬해야 할 듯함.
+ */
+
+public class PRG42746 {
+	public String solution(int[] numbers) {
+		
+		String[] str = new String[numbers.length];
+		
+		for (int i = 0; i < numbers.length; i++) {
+			str[i] = String.valueOf(numbers[i]);
+		}
+		
+		Arrays.sort(str, new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+				return (o2 + o1).compareTo(o1 + o2);
+			}
+		});
+		
+		if (str[0].equals("0")) {
+			return "0";
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (String s : str) {
+			sb.append(s);
+		}
+		
+		return sb.toString();
+    }
+}
