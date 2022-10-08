6a33c842b51f725705b17c45f8e05fbf97894a40
NohHaklim <62823216+NohHaklim@users.noreply.github.com>
2022-10-08 14:57:03 +0900

    점화식을 이용한 동적계획법

---
+++ Programmers/Level_3/PRG43105.java
@@ -0,0 +1,27 @@
+package Level_3;
+
+public class PRG43105 {
+	public int solution(int[][] triangle) {
+        int answer = 0;
+        
+        int[][] dp = new int[triangle.length][triangle.length];
+        dp[0][0] = triangle[0][0];
+        for (int r = 1; r < triangle.length; r++) {
+        	for (int c = 0; c < triangle[r].length; c++) {
+        		if (c == 0) {
+        			dp[r][c] = dp[r - 1][0] + triangle[r][c];
+        		} else if (c == triangle[r].length - 1) {
+        			dp[r][c] = dp[r - 1][r - 1] + triangle[r][c];
+        		} else {
+        			dp[r][c] = Math.max(dp[r - 1][c - 1], dp[r - 1][c]) + triangle[r][c];
+        		}
+        	}
+        }
+        
+        for (int i = 0; i < triangle.length; i++) {
+        	answer = Math.max(dp[triangle.length - 1][i], answer);
+        }
+        
+        return answer;
+    }
+}
